516f4dcb7d6c8f5137a6648fa2cf0bbbaeebdbba
Jeffrey Crowell <jcrowell@google.com>
2015-08-31 15:11:57 -0400

    fix?

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-sudo apt-get update 2>&1 > /dev/null
-sudo apt-get install apache2 g++ python subversion gperf make devscripts fakeroot git curl 2>&1 > /dev/null
+sudo apt-get update
+sudo apt-get install apache2 g++ python subversion gperf make devscripts fakeroot git curl
 mkdir -p ~/bin
 cd ~/bin
-svn co http://src.chromium.org/svn/trunk/tools/depot_tools 2>&1 > dev/null
-mkdir ~/modpagespeed
-cd ~/modpagespeed
+svn co http://src.chromium.org/svn/trunk/tools/depot_tools
+mkdir ~/build_directory
+cd ~/build_directory
 ~/bin/depot_tools/gclient config https://github.com/pagespeed/mod_pagespeed.git --unmanaged --name=src
 git clone https://github.com/pagespeed/mod_pagespeed.git src
 ~/bin/depot_tools/gclient sync --force
 cd src
-make AR.host=`pwd`/build/wrappers/ar.sh AR.target=`pwd`/build/wrappers/ar.sh \
-      BUILDTYPE=Release mod_pagespeed_test pagespeed_automatic_test
-
+make AR.host=`pwd`/build/wrappers/ar.sh AR.target=`pwd`/build/wrappers/ar.sh BUILDTYPE=Release

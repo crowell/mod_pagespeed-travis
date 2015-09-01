5e0e6298585b56d90a4a24d8ca6d8da108f6c5f8
Jeffrey Crowell <jcrowell@google.com>
2015-09-01 13:20:28 -0400

    make scripts executable

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,7 @@
 cd ~/build_directory/src
 ./out/Release/mod_pagespeed_test
 ./out/Release/pagespeed_automatic_test
+find . -name "*.sh" | xargs chmod +x
 cd install
 sudo -E ./ubuntu.sh apache_debug_restart
 sudo -E ./ubuntu.sh apache_vm_system_tests

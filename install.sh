8aea7ba3939d8903713c59fa587602d3c5dd16b7
Jeffrey Crowell <jcrowell@google.com>
2015-09-01 14:05:27 -0400

    netcat needs -q

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update 2>&1 > /dev/null
-sudo apt-get install apache2 g++ python subversion gperf make devscripts fakeroot git curl 2>&1 > /dev/null
+sudo apt-get install apache2 g++ python subversion gperf make devscripts fakeroot git curl netcat-traditional 2>&1 > /dev/null
+sudo mv /bin/nc.traditional /usr/bin/nc
 mkdir -p ~/bin
 cd ~/bin
 svn co http://src.chromium.org/svn/trunk/tools/depot_tools 2>&1 > /dev/null
